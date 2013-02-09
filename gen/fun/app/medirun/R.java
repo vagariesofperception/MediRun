f37e34b4b146e8ee6d8761f7b62308c160518f40
FNU SNU <pprabhu@nat-oitwireless-inside-vapornet100-d-29127.princeton.edu>
2013-02-09 14:07:44 -0500

    Medi mins takes input. Input registered inside controller

--- gen/fun/app/medirun/R.java
+++ gen/fun/app/medirun/R.java
@@ -13,19 +13,21 @@ public final class R {
     public static final class color {
         public static final int medi_color_bkgnd=0x7f050000;
         public static final int medi_color_fgnd=0x7f050001;
+        public static final int medi_mins_color_bkgnd=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int edittext_bg=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int mediDate=0x7f080002;
-        public static final int mediDateMinText=0x7f080007;
         public static final int mediDateVal=0x7f080004;
         public static final int mediEmpty1=0x7f080003;
         public static final int mediEmpty2=0x7f080005;
         public static final int mediEmpty3=0x7f080008;
         public static final int mediLayout=0x7f080000;
+        public static final int mediMinText=0x7f080007;
         public static final int mediMins=0x7f080006;
         public static final int mediTable1=0x7f080001;
         public static final int menu_settings=0x7f08000c;
--- src/fun/app/medirun/MediActivity.java
+++ src/fun/app/medirun/MediActivity.java
@@ -1,23 +1,35 @@
 package fun.app.medirun;
 
-
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class MediActivity extends Activity {
+public class MediActivity extends Activity implements TextView.OnEditorActionListener {
 	private TextView mediDate;
 	private TextView mediDateVal;
+
+	private EditText mediMins;
+	private TextView mediMinText;
+	private Button updateMediBtn;
+
+	private int currentYear, currentMonth, currentDay;
+	private int currentMediMins;
+
+	public final String logTag = "MediActivity";
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,20 +41,51 @@ public class MediActivity extends Activity {
 		if (mediDateVal == null)
 			mediDateVal = (TextView) findViewById(R.id.mediDateVal);
 
-		Intent intent = getIntent();
-		int year = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.YEAR));
-		int month = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.MONTH));
-		int day = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.DAY));
+		if (mediMins == null)
+			mediMins = (EditText) findViewById(R.id.mediMins);
 
+		if (mediMinText == null)
+			mediMinText = (TextView) findViewById(R.id.mediMinText);
+
+		mediMins.setOnEditorActionListener(this);
+
+		// This is to prevent automatic popping of keyboard
+		// for the edittext
+		// http://stackoverflow.com/questions/2496901/android-on-screen-keyboard-auto-popping-up
+		this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+		// Get the date that was clicked and add it to
+		// info row
+		Intent intent = getIntent();
+		currentYear = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.YEAR));
+		currentMonth = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.MONTH));
+		currentDay = Integer.parseInt(intent.getStringExtra(MediRunMainActivity.DAY));
+
+		Log.i(logTag, "onCreate:" +  String.valueOf(currentMonth) + "/" + 
+				String.valueOf(currentDay) + "/" + String.valueOf(currentYear));
 		Date d = null;
 		Calendar cal = GregorianCalendar.getInstance();
-		cal.set(year, month, day);
+		cal.set(currentYear, currentMonth, currentDay);
 		d = cal.getTime();
 		String strDate = DateFormat.format("MMMM dd, yyyy", d).toString();
 		mediDateVal.setText(strDate);
+
+
 	}
 
 	public void updateMediButtonClick(View view) {
-		
+
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+		boolean handled = false;
+		if (actionId == EditorInfo.IME_ACTION_DONE) {
+			String s = v.getText().toString();
+			Log.i(logTag, "Medi Mins:" + s + " on " + String.valueOf(currentMonth) + "/" + 
+					String.valueOf(currentDay) + "/" + String.valueOf(currentYear));
+		}
+		return handled;
 	}
 }
