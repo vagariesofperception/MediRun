0e1cb207e0b8d358d2c038a99b55368dbd181ed2
FNU SNU <pprabhu@nat-oitwireless-inside-vapornet100-d-29127.princeton.edu>
2013-02-09 11:16:42 -0500

    got the button and edittext for mediactivity, does not do anything yet

--- gen/fun/app/medirun/R.java
+++ gen/fun/app/medirun/R.java
@@ -20,13 +20,18 @@ public final class R {
     }
     public static final class id {
         public static final int mediDate=0x7f080002;
+        public static final int mediDateMinText=0x7f080007;
         public static final int mediDateVal=0x7f080004;
-        public static final int mediEmpty=0x7f080003;
+        public static final int mediEmpty1=0x7f080003;
+        public static final int mediEmpty2=0x7f080005;
+        public static final int mediEmpty3=0x7f080008;
         public static final int mediLayout=0x7f080000;
-        public static final int mediTable=0x7f080001;
-        public static final int menu_settings=0x7f080007;
-        public static final int pager=0x7f080005;
-        public static final int pager_title_strip=0x7f080006;
+        public static final int mediMins=0x7f080006;
+        public static final int mediTable1=0x7f080001;
+        public static final int menu_settings=0x7f08000c;
+        public static final int pager=0x7f08000a;
+        public static final int pager_title_strip=0x7f08000b;
+        public static final int updateMediBtn=0x7f080009;
     }
     public static final class layout {
         public static final int activity_medi=0x7f030000;
@@ -39,11 +44,13 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040004;
         public static final int mediDate=0x7f040007;
+        public static final int mediDateMinText=0x7f040008;
         public static final int menu_settings=0x7f040005;
         public static final int title_activity_medi_run_main=0x7f040006;
         public static final int title_section1=0x7f040003;
         public static final int title_section2=0x7f040002;
         public static final int title_section3=0x7f040001;
+        public static final int updateTxt=0x7f040009;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- src/fun/app/medirun/MediActivity.java
+++ src/fun/app/medirun/MediActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -41,4 +42,7 @@ public class MediActivity extends Activity {
 		mediDateVal.setText(strDate);
 	}
 
+	public void updateMediButtonClick(View view) {
+		
+	}
 }
